8bd909633e3ba1eacbf6623d16638731400f4783
Ajinkya Pathare <78788818+apathare9@users.noreply.github.com>
2024-01-18 22:27:32 +0530

    Lecture 6 --> Done

--- Lecture 6/src/components/Body.js
+++ Lecture 6/src/components/Body.js
@@ -1,18 +1,64 @@
+// Business dev,  channel and distribution , product management and enterprise account management,  e commerce 
+// Industry : IT telecom, hardware software and serivces and solution,  e commerce
+
+
+
+
 import { restaurantList } from "../Constants";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
+function filterData(searchInput, restaurants) {
+
+    const filterData = restaurants.filter(
+        (restaurants) => restaurants.info.name.includes(searchInput)
+    );
+
+    return filterData;
+
+}
+
 
 const Body = () => {
+
+    const [restaurants, setRestaurants] = useState(restaurantList);
+    const [searchInput, setSearchInput] = useState("");
+
     return (
+
+        <>
+
+        <div className="search-container" >
+            <input type="text" 
+            className="search-input"    
+            placeholder="Search" 
+            value={searchInput}
+            onChange={(e) => { 
+                setSearchInput(e.target.value);
+            }} 
+            />
+            <button className="search-btn" 
+            onClick={() => {
+                // need to filter the data
+                const data = filterData(searchInput, restaurants);
+
+                // update the state - restraunts
+                setRestaurants(data);
+            }}> Search </button>
+        </div>
+        
+
         <div className="restaurant-list">
             {
-                restaurantList.map((restaurantList) => {
+                restaurants.map((restaurantList) => {
                     return <RestaurantCard {...restaurantList.info} key={restaurantList.info.id} />
                 })
             }
         </div>
+        </>
 
 
     );
 }
 
-export default Body;
+export default Body;    
