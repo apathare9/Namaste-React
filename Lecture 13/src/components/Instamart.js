fbe403e4b6b46da6873fb0420cd106344f928aab
Ajinkya Pathare <78788818+apathare9@users.noreply.github.com>
2024-01-26 23:28:51 +0530

    13.........

---
+++ Lecture 13/src/components/Instamart.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const Section = ({ title, description }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div className="border border-black p-2 m-2">
+      <h3>{title}</h3>
+
+      {isVisible ? (
+        <button
+          onClick={() => setIsVisible(false)}
+          className="cursor-pointer underline"
+        >
+          Hide
+        </button>
+      ) : (
+        <button
+          onClick={() => setIsVisible(true)}
+          className="cursor-pointer underline"
+        >
+          Show
+        </button>
+      )}
+
+      {isVisible && <p>{description}</p>}
+    </div>
+  );
+};
+
+const Instamart = () => {
+  return (
+    <div>
+      <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
+      <Section
+        title={"About Instamart"}
+        description={"This is the about section of Instamart"}
+      />
+
+      <Section
+        title={"Team Instamart"}
+        description={
+          "This is the team section of Instamart. The team has 50 members...."
+        }
+      />
+    </div>
+  );
+};
+
+export default Instamart;
